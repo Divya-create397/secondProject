021225f99fa882b5b20575a2900ac63296609dce
Divya-create397 <divyahm01@gmail.com>
2024-08-19 17:59:37 +0530

    Update class2.java

--- com.secondProject.Framework/src/test/java/secondProjectPackage1/class2.java
+++ com.secondProject.Framework/src/test/java/secondProjectPackage1/class2.java
@@ -5,6 +5,7 @@ public class class2
 	public static void main(String[] args)
 	{
 		System.out.println("New class is added by Divya2");
+		System.out.println("New class is added by Divya2 is modified by global Repo");
 
 	}
 
