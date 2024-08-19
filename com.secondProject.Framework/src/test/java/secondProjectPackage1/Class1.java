48dc6371248b70312f49c067572d7478f480ce0e
Divya-create397 <divyahm01@gmail.com>
2024-08-19 14:49:26 +0530

    Update Class1.java

--- com.secondProject.Framework/src/test/java/secondProjectPackage1/Class1.java
+++ com.secondProject.Framework/src/test/java/secondProjectPackage1/Class1.java
@@ -5,6 +5,7 @@ public class Class1
 	public static void main(String[] args)
 	{
 		System.out.println("This is the second project class one");
+		System.out.println("This is the message we are adding from Git");
 	}
 
 }
