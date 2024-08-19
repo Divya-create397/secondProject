5b4d898c0702c13b28c4436602a2e8bc30d15801
DeepakTheGreat <DeepakTheGreat@DeepakLappy>
2024-08-19 17:49:40 +0530

    Added the changes made in working directory to local repository

--- com.secondProject.Framework/src/test/java/secondProjectPackage1/Class1.java
+++ com.secondProject.Framework/src/test/java/secondProjectPackage1/Class1.java
@@ -7,6 +7,7 @@ public class Class1
 		System.out.println("This is the second project class one");
 		System.out.println("This is the message we are adding from Git");
 		System.out.println("Second time we are pushing to github");
+		System.out.println("Made the changes in working directory");
 	}
 
 }
